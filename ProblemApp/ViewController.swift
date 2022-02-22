713108846841229b2d6de6367c4adc647426fb50
MusaEnesKinik <kinikenes76@gmail.com>
2022-02-22 14:18:15 +0300

    Problem App IOS Commit

--- ProblemApp/ViewController.swift
+++ ProblemApp/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonClicked(_ sender: Any) {
+        
+        resultLabel.text = String(Int(textField.text!)! * 10)
+        
+    }
+    
 }
 
